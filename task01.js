d01e5c82936490d91017f279b771f27c0b87dd23
madanadi0305 <madanadi0305@gmail.com>
2020-09-29 14:55:46 +0000

    Update task01.js

--- task01.js
+++ task01.js
@@ -1,3 +1,16 @@
+var obj = {
+  types: {
+    binary: [],
+    decimal: [],
+    numberData: {
+        primeNumbers: [1,2,3,4,5],
+        fibonnaci: []
+    }
+   
+  }
+}
+
+
 function task01(obj){
 var prime_array=[];
 var primeNumberValues=obj.primeNumber;
