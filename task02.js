f7128c41ca1972586d12bfa2bf604d0065accbf4
madanadi0305 <madanadi0305@gmail.com>
2020-09-30 07:07:24 +0000

    Update task02.js

--- task02.js
+++ task02.js
@@ -11,15 +11,15 @@ var nestedData = {
 }
 
 function task02(nestedData){
-var fibonacci_array=[];
-var fibonacciArray=nestedData.innerData.numberData.fibonacci;
-for(var i=0;i<fibonacciArray.length;i++){
+var fib_array=[];
+var fibonnacci_array=nestedData.innerData.numberData.fibonnaci;
+for(var i=0;i<fibonnacci_array.length;i++){
 if(i%2===0){
-fibonacci_array.push(fibonacciArray[i]);
-}
-}
-return fibonacci_array;
+fib_array.push(fibonnacci_array[i]);
+
 }
 
-
+}
+return fib_array;
+}
 module.exports=task02;
