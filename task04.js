ad1f5479201b04db5109382b11c60fe46b0512dc
madanadi0305 <madanadi0305@gmail.com>
2020-09-30 16:54:55 +0000

    Update task04.js

--- task04.js
+++ task04.js
@@ -34,7 +34,7 @@ countryObj[countryName]=newObj;
 var keys=Object.keys(countryObj);
 var keyname=keys[keys.length-1];
 //return keyname;
-var obj=countryObj[countryKey];
+var obj=countryObj[keyname];
 return obj;
 }
 module.exports=task04;
