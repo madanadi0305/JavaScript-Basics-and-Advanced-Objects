1a76807decb01188248d8192731678f4c4710ca8
madanadi0305 <madanadi0305@gmail.com>
2020-09-30 11:46:09 +0000

    Update task02.js

--- task02.js
+++ task02.js
@@ -14,7 +14,10 @@ function task02(nestedData){
 var fib_array=[];
 
 var fibonnacci_array=nestedData.innerData.numberData.fibonnaci;
+if(fibonnacci_array===[]||fibonnacci_array===null){
+return -1;
 
+}
 for(var i=0;i<fibonnacci_array.length;i++){
 if(i%2===0){
     
