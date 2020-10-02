002b9166965a80131027e150ae700286f2ce8531
madanadi0305 <madanadi0305@gmail.com>
2020-10-02 05:55:37 +0000

    Update Wiki/16-task06.md, task06.js files

---
+++ task06.js
@@ -0,0 +1,46 @@
+var arr = [
+  { id: 15 },
+  { id: -1 },
+  { id: 0 },
+  { id: 3 },
+  { id: 12.2 },
+  {},
+  { id: null },
+  { id: NaN },
+  { id: "undefined" }
+];
+
+
+
+function task06(arr){
+var newObj={};
+var prop1;
+var prop2;  
+
+var prop=[];  
+for(var i=0;i<arr.length;i++){
+var value=arr[i]["id"];
+
+prop.push(value);
+  
+}
+
+prop1=prop.filter(function(x){return ((x!==null)&&(x!==0)&&(x!==undefined)&&(x!==NaN)&&(x!=="undefined"))});  
+for(var i=0;i<prop1.length;i++){
+if(isNaN(prop1[i])){
+prop1.splice(i,1);
+}
+}
+prop2=prop.filter(function(x){return(x===null||isNaN(x)||value==="undefined"||value===undefined||value===0)});  
+
+
+console.log(prop1);  
+console.log(prop2);  
+newObj.prop1=prop1;
+newObj.prop2=prop2;
+
+return newObj;  
+}
+
+
+module.exports=task06;
