afa378cf04b36168f5975d080ff874c37bef63f9
madanadi0305 <madanadi0305@gmail.com>
2020-10-02 14:49:56 +0000

    Update task05.js, Wiki/15-task05.md files

---
+++ task05.js
@@ -0,0 +1,34 @@
+var library = [
+    {
+        title: "",
+        author: "",
+        readingStatus:""
+    },
+    {
+        title:"" ,
+        author:"" ,
+        readingStatus:""
+    },
+    ];
+
+    function task05(library){
+     var true_array=[];
+     var false_array=[];
+     var resultObj={};
+     for(var i=0;i<library.length;i++){
+     var readingStatus=library[i].readingStatus;
+     if(readingStatus===true){
+     true_array.push(library[i].title);
+     true_array.push(library[i].author);
+
+     }
+     else{
+         false_array.push(library[i].title);
+         false_array.push(library[i].author);
+     }
+     }
+     resultObj.prop1=true_array;
+     resultObj.prop2=false_array;
+     return resultObj;
+    }
+    module.exports=task05;
