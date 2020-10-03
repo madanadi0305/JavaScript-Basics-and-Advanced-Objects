410410d730dc533af46fc3f841d93a3104c111c2
madanadi0305 <madanadi0305@gmail.com>
2020-10-03 13:32:31 +0000

    Update Wiki/21-task11.md, task11.js files

---
+++ task11.js
@@ -0,0 +1,22 @@
+var recipe = {
+  title: "mole",
+  servings: [2, 5, 7, 9, 7, 9, 6, 5, 4, 4],
+  ingredients: ["cumin", "cinnamon", "cocoa"]
+};
+
+function task11(recipe){
+var servings_array=recipe.servings;
+var repeat_array=[];  
+for(var i=0;i<servings_array.length;i++){
+var elem=servings_array[i];
+  for(var j=i+1;j<servings_array.length;j++){
+   if(servings_array[j]===elem){
+   repeat_array.push(elem);
+   }
+
+}
+
+}
+  return repeat_array;
+}
+module.exports=task11;
