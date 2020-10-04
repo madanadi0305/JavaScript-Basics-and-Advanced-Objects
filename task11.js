a771a47bef9230e94b9fdae99bcf082bf0889fc6
madanadi0305 <madanadi0305@gmail.com>
2020-10-04 07:33:59 +0000

    Update task11.js

--- task11.js
+++ task11.js
@@ -7,7 +7,7 @@ var recipe = {
 function task11(recipe){
 var list=recipe.servings;
 var repeat=[];
-var count;  
+var count,isOk;  
 for(var i=0;i<list.length;i++){
 var val=list[i];
 count=1;  
@@ -15,13 +15,16 @@ for(var j=i+1;j<list.length;j++){
 if(val===list[j]){
 if(count>=2){break;}  
 repeat.push(list[j]);
-count=count+1;  
+count=count+1;
+isOk=1;  
 //console.log(count);  
 }
 }  
 }  
 //console.log(repeat);
-return repeat;  
+if(isOk===1)
+{return repeat;}
+else{return -1;}  
 }
 
 module.exports=task11;
