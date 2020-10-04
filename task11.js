91e9cadb892950fe60a115b39e2c665ba80dc732
madanadi0305 <madanadi0305@gmail.com>
2020-10-04 10:09:39 +0000

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,33 +1,39 @@
 var recipe = {
   title: "mole",
-  servings: [],
-  ingredients: ["cumin", "cinnamon", "cocoa"]
+  servings: [2,1,1,1,2,3,10,10,10,9,2,3,3,5,7,9,7,9,6,5,4,4],
+  //servings:[2, 5, 7, 9, 7, 9, 6, 5, 4, 4],
+  ingredients: ["cumin", "cinnamon", "cocoa"]
 };
 
 function task11(recipe){
 var list=recipe.servings;
-
-console.log("Original List:",list);
 var repeat=[];
-var count,isOk;  
+var repeat_1=[];  
+var count,k;  
+console.log("Original List:",list);  
 for(var i=0;i<list.length;i++){
 var val=list[i];
-count=1;  
-for(var j=i+1;j<list.length;j++){
-if(val===list[j]){
-if(count>=2){break;}  
-repeat.push(list[j]);
+count=0;
+ for(var j=i;j<list.length;j++){
+if(list[j]===val){
 count=count+1;
-//isOk=1;  
-//console.log(count);  
-}
-}  
-}  
-console.log("Returned List:",repeat);
+ k=list[j];
 
-return repeat;
+
+
   
+ if(count>=2){
+   repeat.push(k);
+   break;
+ }
 }
 
+}
+  
+}  
+console.log("New List:",repeat);  
+return repeat;  
+}
 module.exports=task11;
-
+//var a=task11(recipe);
+//console.log(a);
