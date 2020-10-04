c2f1db135dfdc27d49c0d9cd1916474465523fe4
madanadi0305 <madanadi0305@gmail.com>
2020-10-04 10:38:49 +0000

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,7 +1,8 @@
 var recipe = {
   title: "mole",
-  servings: [],
+  //servings: [2,1,1,1,2,3,10,10,10,9,2,3,3,5,7,9,7,9,6,5,4,4],
   //servings:[2, 5, 7, 9, 7, 9, 6, 5, 4, 4],
+   servings:{},
   ingredients: ["cumin", "cinnamon", "cocoa"]
 };
 
@@ -9,23 +10,26 @@ function task11(recipe){
 var list=recipe.servings;
 var repeat=[];
 var repeat_1=[];  
-var count,k;  
+var count,k;
+var max;  
 console.log("Original List:",list);  
+
 for(var i=0;i<list.length;i++){
 var val=list[i];
 count=0;
  for(var j=i;j<list.length;j++){
 if(list[j]===val){
-count=count+1;
+
  k=list[j];
 
 
 
   
- if(count>=2){
+ if(count===2){
    repeat.push(k);
    break;
  }
+  count=count+1;
 }
 
 }
@@ -34,6 +38,5 @@ count=count+1;
 console.log("New List:",repeat);  
 return repeat;  
 }
-module.exports=task11;
 //var a=task11(recipe);
 //console.log(a);
