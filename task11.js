18ba57b59913b9941d7ff1b024c831be53a7f435
madanadi0305 <madanadi0305@gmail.com>
2020-10-06 05:18:02 +0000

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,43 +1,48 @@
-var recipe = {
+  var recipe = {
   title: "mole",
-  //servings: [2,1,1,1,2,3,10,10,10,9,2,3,3,5,7,9,7,9,6,5,4,4],
-  //servings:[2, 5, 7, 9, 7, 9, 6, 5, 4, 4],
-   servings:{},
-  ingredients: ["cumin", "cinnamon", "cocoa"]
+  servings: [2, 5, 7, 9, 7, 9, 6, 5, 4, 4],
+  //servings:[2,1, 1, 1, 2, 3, 10,10, 10, 9, 2, 3, 3,  5,7,9, 7, 9, 6, 5,4,4],
+  ingredients: ["cumin", "cinnamon", "cocoa"]
 };
 
 function task11(recipe){
 var list=recipe.servings;
-var repeat=[];
-var repeat_1=[];  
-var count,k;
-var max;  
-console.log("Original List:",list);  
-
+var val;
+console.log(list);
+var counts={};
+var list2=[];
+var list3=[];
 for(var i=0;i<list.length;i++){
-var val=list[i];
-count=0;
- for(var j=i;j<list.length;j++){
-if(list[j]===val){
-
- k=list[j];
+val=list[i];
 
 
+  if(!counts[val]){
+  counts[val]=1;
+  }
+  else{
+  counts[val]=counts[val]+1;
 
-  
- if(count===2){
-   repeat.push(k);
-   break;
- }
-  count=count+1;
 }
 
 }
-  
-}  
-console.log("New List:",repeat);  
-return repeat;  
+console.log(counts);
+for(var Val in counts){
+if(counts[Val]>=2){
+list2.push(Val);
+}
+console.log(list2);
+}
+for(var i=0;i<list.length;i++){
+for(var j=0;j<list2.length;j++){
+if(list2[j]===list[i]){
+list3.push(list[i]);
+
+}
+
+}
+}
+//console.log(list3);
+return list3;
 }
 module.exports=task11;
-//var a=task11(recipe);
-//console.log(a);
+//task11(recipe);
