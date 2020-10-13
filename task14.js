ab57375f8483438fee4086f01958b1d9571c2060
madanadi0305 <madanadi0305@gmail.com>
2020-10-13 16:53:39 +0000

    Update task14.js

--- task14.js
+++ task14.js
@@ -26,64 +26,57 @@ var data = {
     }
   ]
 };
+var str="";
 
 function task14(data){
-var directory=[];
-var root=data.name;
-var val;
-var string1="";
+  var str2=start(data);
+  return str2;
+}
+function start(objNode){
+var objContent;
+if(objNode.name){
+  str=str+objNode.name+"\n";
+}
 
-string1=string1+root+"\n";
-var contents=data.contents;
 
-
-
-for(var i=0;i<contents.length;i++){
-
-var elem=contents[i];
-
-var keys=Object.keys(elem);
-for(var i=0;i<keys.length;i++){
-
-   var key=keys[i];
-  if(key==='name'){
+if(typeof objNode.contents==='object'){
+  objNode.contents.forEach(function(next){
     
-    string1+elem[key]+"\n";
- 
-  }
-  
- if(key==='contents'){
-   var innerElem=elem[key];
-  var innerKeys=Object.keys(innerElem);
-  string1=string1+innerElem[innerKeys].name+"\n"+string1+innerElem[innerKeys].contents[0]+"\n";
- }
-}
-
-}
-var nextContent=contents[1];
-var keysNextContent=Object.keys(nextContent);
-for(var i=0;i<keysNextContent.length;i++){
-var keyNext=keysNextContent[i];
-if(keyNext==='name'){
-  string1=string1+nextContent[keyNext]+"\n";
-}
-if(keyNext==='contents'){
-  var contentElem=nextContent[keyNext];
-  var contentElemKeys=Object.keys(contentElem);
-  string1=string1+contentElem[contentElemKeys].name+"\n";
-
- var contentElem2=contentElem[contentElemKeys].contents;
- var contentElemKeys2=Object.keys(contentElem2);
- string1=string1+contentElem2[contentElemKeys2].name+"\n";
- string1=string1+contentElem2[contentElemKeys2].contents+"\n";
    
+   if(next.contents){
+   //  str=str+objContent.contents+"\n";
+  
+   objContent=next.contents;
+  
+   }
+   
+     start(next);
+  
+
+  });
+
+  
+   str=str+objContent+"\n";
+  
+ 
 }
 
+var array=str.split("\n");
+var array1=[];
+//console.log(array);
+array.forEach(function(elem){
+if((elem!==" ") && (elem!=='undefined') && (elem!=="[object Object]")){
+  array1.push(elem);
+  array1.push("\n");
 }
 
+});
+var str1=array1.join("");
+return str1;
 
-return string1;
+
 
 }
+
 module.exports=task14;
 
